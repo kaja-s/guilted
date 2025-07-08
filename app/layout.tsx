d191c1ca5ac74f2ef50a1a2a1c0587e8947bdba7
Kaja Skerlj <kaja.skerlj@gmail.com>
2025-07-08 11:21:07 +0200

    Vercel analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Script from "next/script";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <Analytics />
         <Script
           id="clarity-script"
           strategy="afterInteractive"
